041c467461b890cb0b553004927485836ed843e4
SULFYY <70634014+ChTheo@users.noreply.github.com>
2025-09-13 23:09:51 +0200

    Create backup.py
    ajout dag backup database

---
+++ dags/backup.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+with DAG(
+    dag_id="backup_database",
+    schedule_interval="0 4 * * *", 
+    start_date=datetime(2025, 1, 1),
+    catchup=False,
+    tags=["database"],
+) as dag:
+
+    backup_task = BashOperator(
+        task_id="run_postgres_backup",
+        bash_command="""
+        mkdir -p /opt/airflow/backups &&
+        pg_dump -U postgres -h postgres analyse_cartes > /opt/airflow/backups/backup_$(date +%Y%m%d_%H%M%S).sql
+        """,
+        env={"PGPASSWORD": "1"} 
+    )
+
+    backup_task
