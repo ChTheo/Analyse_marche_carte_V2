f0dbb0df8a41163cb976900088f2d8e8c5a9ceab
SULFYY <70634014+ChTheo@users.noreply.github.com>
2025-09-13 23:31:35 +0200

    Update backup.py

--- dags/backup.py
+++ dags/backup.py
@@ -1,10 +1,10 @@
 from airflow import DAG
 from airflow.operators.bash import BashOperator
-from datetime import datetime, timedelta
+from datetime import datetime
 
 with DAG(
     dag_id="backup_database",
-    schedule_interval="0 4 * * *", 
+    schedule_interval="0 4 * * *",  
     start_date=datetime(2025, 1, 1),
     catchup=False,
     tags=["database"],
@@ -14,9 +14,9 @@ with DAG(
         task_id="run_postgres_backup",
         bash_command="""
         mkdir -p /opt/airflow/backups &&
-        pg_dump -U postgres -h postgres analyse_cartes > /opt/airflow/backups/backup_$(date +%Y%m%d_%H%M%S).sql
+        pg_dump -U postgres -h 10.0.0.2 analyse_cartes > /opt/airflow/backups/backup_$(date +%Y%m%d_%H%M%S).sql
         """,
-        env={"PGPASSWORD": "1"} 
+        env={"PGPASSWORD": "1"}  #
     )
 
     backup_task
